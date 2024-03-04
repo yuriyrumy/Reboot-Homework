7e7d9f18e440d1f193aaa1f48227c71b9c1f6521
Yuriy Rum <yuriywwh@gmail.com>
2024-03-04 15:56:27 +0200

    Add oop/task6.py file

---
+++ python_advanced/oop/task6.py
@@ -0,0 +1,26 @@
+# Розробіть клас Counter, який буде мати атрибут класу count. Створіть метод get_count який буде повертати атрибут
+# класу count. Кожного разу, коли ми створюємо екземпляр класу Counter ми повинні збільшувати атрибут класу counter на 1,
+# тим самим зберігаючи там кількість екземплярів створених для цього класу. А під час виклику метода get_count ми повинні
+# фіксувати кількість викликів даного методу для кожного екземпляру.
+
+class Counter:
+    count = 0
+
+    def __init__(self):
+        Counter.count += 1
+        self.value = 0
+
+    def get_count(self):
+        return self.count
+
+    def method_called(self):
+        self.value += 1
+        return self.value
+
+
+c1 = Counter()
+c2 = Counter()
+
+result = c1.get_count()
+print(result)  # 2
+print(c1.method_called())  # 1
