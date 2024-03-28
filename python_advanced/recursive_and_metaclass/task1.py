362e15478a86b34e028c32593c67bfb38d01e7ad
Yuriy Rum <yuriywwh@gmail.com>
2024-03-28 14:11:54 +0200

    Add task1.py

---
+++ python_advanced/recursive_and_metaclass/task1.py
@@ -0,0 +1,11 @@
+# Напишіть рекурсивну функцію, яка обчислює суму цифр заданого числа.
+
+def recursive_sum(number):
+    if number < 10:
+        return number
+
+    return number % 10 + recursive_sum(number // 10)
+
+
+result = recursive_sum(1234)
+print(result)
